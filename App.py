b0506a9942b9877f951f08ff6dd64e9e0c7d18ed
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2024-08-15 21:07:51 -0500

    Update App.py
    Add prey and predator population models with plot_population_time function. Created a new canvas in the scrollPanel.

--- App.py
+++ App.py
@@ -27,6 +27,49 @@ class MyFrame(customtkinter.CTkScrollableFrame):
         predator_population = [predator_initial]
         marker_size = 2
 
+        def prey(n):
+            prey_model = prey_population[n-1]
+            predator_model = predator_population[n-1]
+            return prey_model + a * prey_model * (1 - prey_model / K) - b * prey_model * predator_model
+        def predator(n):
+            prey_model = prey_population[n-1]
+            predator_model = predator_population[n-1]
+            return predator_model + f * b * prey_model * predator_model - c * predator_model
+        
+        def model(): 
+            for i in range(1,cycles):
+                prey_population.append(prey(i))
+                predator_population.append(predator(i))
+        prey_e = c / (b * f)
+        predator_e = (a / b) * (1 - prey_e / K)
+
+        def plot_population_time():
+
+            plt.figure(1)
+
+            plt.axhline(y=predator_e, color='red', linestyle='--')
+
+            plt.axhline(y=prey_e, color='red', linestyle='--')
+
+            plt.plot(np.arange(cycles), prey_population, label='Prey Population', markersize=marker_size)
+
+            plt.plot(np.arange(cycles), predator_population, label='Predator Population', markersize=marker_size)
+
+            plt.ylabel('Population')
+
+            plt.xlabel('Time')
+
+            plt.title('Predator and Prey Populations over Time')
+
+            plt.legend()
+            return plt.gcf()
+        model()
+        figure = plot_population_time()
+
+        canvas = FigureCanvasTkAgg(figure, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=0, column=0, sticky="nsew")
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -36,6 +79,9 @@ class App(customtkinter.CTk):
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
+
+        self.MyFrame = MyFrame(self)
+        self.MyFrame.grid(row=0, column=0, sticky='nsew')
 
 
 
